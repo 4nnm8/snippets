891a2ca2eb842f2fad48adc8cd71854bda4b036b
4nnm8 <37158166+4nnm8@users.noreply.github.com>
2024-04-14 19:02:41 +0000

    Create smooth-scroll-to.js

---
+++ smooth-scroll-to.js
@@ -0,0 +1,10 @@
+document.addEventListener("click", event => {
+	const anchor = event.target.closest('a[href^="#"]');
+	if (anchor) {
+		event.preventDefault();
+		document.getElementById(anchor.href.split("#")[1]).scrollIntoView({
+      behavior: window.matchMedia("(prefers-reduced-motion: reduce)").matches ? "instant" : "smooth",
+      block: start /* or center, end, nearest */
+    })
+	}
+});

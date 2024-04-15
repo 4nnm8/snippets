aa6254b7488855142e721a86f5494d26c5adc4c4
4nnm8 <37158166+4nnm8@users.noreply.github.com>
2024-04-15 13:49:52 +0000

    Update back-to-top-with-progression-button.js

--- back-to-top-with-progression-button.js
+++ back-to-top-with-progression-button.js
@@ -1,7 +1,12 @@
+const toTopLink = document.getElementById("link-to-top");
+
 document.querySelector(".container").addEventListener("scroll", function() {
-	const top = this.scrollTop,
-		    link = document.querySelector("a#link-to-top");
-	link.style.setProperty("--angle", Math.round(top * 360 / this.offsetHeight) + "deg");
-});
+  const mainHeight = document.querySelector("main").offsetHeight,
+        progression = this.scrollTop / mainHeight;
+  toTopLink.style.setProperty("--angle", progression * 360 + "deg");
+  
+  // Optionnal: to see the percent of scroll progression in the button instead of a triangle
+  toTopLink.dataset.percent = Math.round(progression * 100);
+})
 
 // DEMO AND STYLES HERE : https://codepen.io/Anonyme-the-builder/pen/NWmXKxV

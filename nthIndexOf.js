9578987f20af5e2a61607f2a35edb209396fadbb
4nnm8 <37158166+4nnm8@users.noreply.github.com>
2024-04-30 21:36:10 +0000

    Update nthIndexOf.js

--- nthIndexOf.js
+++ nthIndexOf.js
@@ -1,18 +1,24 @@
-Array.prototype.nthIndexOf = function(pattern, n) {
-  if (n) {
-    let i = -1;
-    while (n-- && i++ < this.length) {
-      i = this.indexOf(pattern, i);
-      if (i < 0) break;
-    }
-	return i;
-  } else {
-    const indexes = [];
-    for(let i = 0 ; i < this.length ; i++) {
-        if (this[i] === pattern) indexes.push(i);
+Array.prototype.nthIndexOf = function(pattern, nth) {
+	const   isAnArray = Array.isArray(pattern),
+		isNbOrStr = pattern.substring || pattern.toFixed;
+	if (nth) {
+		let index = -1;
+		while (nth-- && index++ < this.length) {
+			index = this.indexOf(pattern, index);
+			if (index < 0) break;
+		}
+		return index
+	} else {
+		const indexes = [];
+		for (let i = 0 ; i < this.length ; i++) {
+			if (isAnArray) {
+				if (pattern.includes(this[i])) 	indexes.push(i);
+			} else {
+				if (this[i] === pattern) indexes.push(i);
+			}
+		}
+		return indexes.length === 0 ? -1 : indexes.length === 1 ? indexes[0] : indexes;
 	}
-    return indexes;
-  }
 }
 
 // USAGE:
@@ -22,5 +28,8 @@ const arr = ["q","w","e","r","t","y","q","e"];
 // Get all indexes of the letter "e"
 arr.nthIndexOf("e");    // OUTPUT: [2,7]
 
+// Get all indexes of letter "e" and "q"
+arr.nthIndexOf(["e","q"]);	// OUTPUT: [0,2,6,7]
+
 // With a second parameter, get the index of the nth letter "q" (second one in this example)
-arr.nthIndexOf("q",2);  // OUTPUT: 6
+arr.nthIndexOf("q", 2);  // OUTPUT: 6

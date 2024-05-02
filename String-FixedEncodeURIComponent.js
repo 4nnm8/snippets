5757fc90a714ee60b594df7a1ca6ed729c9a83ee
4nnm8 <37158166+4nnm8@users.noreply.github.com>
2024-05-02 12:26:42 +0000

    Update String-FixedEncodeURIComponent.js

--- String-FixedEncodeURIComponent.js
+++ String-FixedEncodeURIComponent.js
@@ -1,3 +1,11 @@
-String.prototype.FixedEncodeURIComponent() {
+String.prototype.FixedEncodeURIComponent = function() {
   return encodeURIComponent(this).replace(/[!'()*]/g, c => "%" + c.charCodeAt(0).toString(16))
 }
+
+// USAGE:
+
+"https://github.com/4nnm8/snippets/blob/main/String-FixedEncodeURIComponent.js".FixedEncodeURIComponent();
+
+// OUTPUT:
+
+"https%3A%2F%2Fgithub.com%2F4nnm8%2Fsnippets%2Fblob%2Fmain%2FString-FixedEncodeURIComponent.js"
